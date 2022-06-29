c1363d7c550faf7408f778a7a90cff1d1657b543
GubarievaAnna <annagubarieva@gmail.com>
2022-06-29 20:58:48 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+
+function createBlockWithImage({ preview, original, description }) {
+  return `<li><a class="gallery__item" href = "${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a></li>`;
+}
+
+galleryListEl.insertAdjacentHTML(
+  "beforeend",
+  galleryItems.map(createBlockWithImage).join("")
+);
+
+const gallery = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+  showCounter: false,
+  widthRatio: 0.6,
+});
