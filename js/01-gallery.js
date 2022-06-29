0248836bcd76355eb253c3b2339ffa0d17c777df
GubarievaAnna <annagubarieva@gmail.com>
2022-06-29 19:47:29 +0300

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,11 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
+let instance;
 
 function createBlockWithImage({ preview, original, description }) {
   return ` <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
+        <a class="gallery__link" href = "${original}">
           <img
             class="gallery__image"
             src="${preview}"
@@ -16,30 +17,32 @@ function createBlockWithImage({ preview, original, description }) {
       </div>`;
 }
 
-const blocksWithImages = galleryItems.map(createBlockWithImage).join("");
-
-galleryEl.insertAdjacentHTML("beforeend", blocksWithImages);
+galleryEl.insertAdjacentHTML(
+  "beforeend",
+  galleryItems.map(createBlockWithImage).join("")
+);
 
 galleryEl.addEventListener("click", onImageClick);
 
 function onImageClick(event) {
-  event.preventDefault;
-  if (event.target.nodeName !== "img") {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
     return;
   }
+  const urlEl = event.target.dataset.source;
+
+  instance = basicLightbox.create(`
+      <img src="${urlEl}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", onKeyboardPress);
 }
 
-//   const instance = basicLightbox.create(
-//     `<img src="${event.target.dataset.source}" width="800" height="600">`
-//   );
-
-//   instance.show(() => console.log("lightbox now visible"));
-
-//   basicLightbox
-//     .create(
-//       `<img width="1400" height="900" src="${event.target.dataset.source}"> `
-//     )
-//     .show();
-// }
-
-//
+function onKeyboardPress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+  document.removeEventListener("keydown", onKeyboardPress);
+}
