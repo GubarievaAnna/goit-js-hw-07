82715337a4fc29548e61fcc14905c29e022a6e76
GubarievaAnna <annagubarieva@gmail.com>
2022-06-29 17:51:44 +0300

    gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createBlockWithImage({ preview, original, description }) {
+  return ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+const blocksWithImages = galleryItems.map(createBlockWithImage).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", blocksWithImages);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault;
+  if (event.target.nodeName !== "img") {
+    return;
+  }
+}
+
+//   const instance = basicLightbox.create(
+//     `<img src="${event.target.dataset.source}" width="800" height="600">`
+//   );
+
+//   instance.show(() => console.log("lightbox now visible"));
+
+//   basicLightbox
+//     .create(
+//       `<img width="1400" height="900" src="${event.target.dataset.source}"> `
+//     )
+//     .show();
+// }
+
+//
